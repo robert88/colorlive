d86685827d477f4e1f884e58bcc8e7742bc249e5
robert yin <robert yin>
2017-04-01 08:57:10 +0800

    s

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,8 @@
 	var maxHeight = 889;
 	var minHeight = 400;
 
+	var maxWidth = 900;
+	var minWidth = 300;
 	//等比
 	function getResize(max, min, cur, maxCur, minCur) {
 		return (cur - min) * (maxCur - minCur) / (max - min) + minCur;
@@ -37,7 +39,11 @@
 
 	function changeRootrem() {
 		var h = getRagen($(window).height(), maxHeight, minHeight);
-		var remroot = getResize(maxHeight, minHeight, h, 100, 75)
+		var remHroot = getResize(maxHeight, minHeight, h, 100, 75);
+		var w = getRagen($(window).width(), maxWidth, minWidth);
+		var remWroot = getResize(maxWidth, minWidth, w, 100, 75);
+		var remroot = Math.min(remHroot,remWroot);
+		console.log(remroot)
 		if (preh == h) {
 			return remroot;
 		}
@@ -60,36 +66,69 @@
 
 	$(function () {
 		function renderData(data) {
+			if(data.eventArray){
+				data.eventArray.sort(function (a,b) {
+					return a.event_time-b.event_time>0?-1:1;
+				});
+			}
+			
 			new Vue({
 				el: "#colorlivePage",
 				data: data,
 				methods: {
 					getClass: function (idx) {
-						console.log(idx);
-						var map = [{angle: 170, size: "size4"},
-							{angle: 190, size: "size1"},
-							{angle: 167, size: "size3"},
+						// console.log(idx);
+						var map = [
+							{angle: 180, size: "size1"},
 							{angle: 310, size: "size2"},
+							{angle: 267, size: "size3"},
+							{angle: 90, size: "size4"},
 							{angle: 20, size: "size5"},
-							{angle: 90, size: "size6"},]
+							{angle: 170, size: "size6"}]
 						return map[idx].size;
 					},
 					getAngle: function (idx) {
-						var map = [{angle: 170, size: "size4"},
-							{angle: 190, size: "size1"},
-							{angle: 167, size: "size3"},
+						var map = [
+							{angle: 180, size: "size1"},
 							{angle: 310, size: "size2"},
+							{angle: 267, size: "size3"},
+							{angle: 90, size: "size4"},
 							{angle: 20, size: "size5"},
-							{angle: 90, size: "size6"},]
+							{angle: 170, size: "size6"}]
 						return map[idx].angle;
 					}
 				},
+
 				filters: {
 					"toBgUrl": function (val) {
 						return val ? ('background:url(' + val + ') no-repeat;background-size:100% 100%;') : "";
 					},
+					getColor:function (val){
+					var map = {"0":"rgb(56,101,138)",
+						"1":"rgb(239,169,53)",
+						"2":"rgb(28,186,121)",
+						"3":"rgb(49,171,244)",
+						"4":"rgb(35,114,204)",
+						"5":"rgb(31,193,194)",
+						"6":"rgb(183,222,238)",
+						"7":"rgb(210,93,178,)",
+						"8":"rgb(229,190,128)",
+						"9":"rgb(237,84,87)",
+						"10":"rgb(194,226,60)",
+						"11":"rgb(151,76,160)",
+						"12":"rgb(84,135,251)",
+						"13":"rgb(249,120,73)",
+						"14":"rgb(133,214,40)",
+						"15":"rgb(253,245,151)",
+						"16":"rgb(252,202,176)",
+						"17":"rgb(84,198,252)",
+						"18":"rgb(226,134,93)",
+						"19":"rgb(244,120,170)"
+					}
+						return "background:"+map[val]+";"
+					},
 					getLabel: function (val) {
-						return "images/icon/" + (val | 0) + "@2x.png";
+						return "static/images/icon/" + (val | 0) + "@2x.png";
 					},
 					"moonToStr": function (val) {
 						var time_moon_map = {
@@ -118,12 +157,12 @@
 
 		/*随机改变字体大小和颜色*/
 		function roundTextFont($this) {
-			var fontSizeArr = [16, 18, 30, 24, 20, 26];
+			var fontSizeArr = [1, 1.125, 1.875, 1.5, 1.25, 1.625];
 			var fontSize = fontSizeArr[Math.floor(Math.random() * fontSizeArr.length)];
 			var opacity = fontSize / 30;
 			$this.css({
 				opacity: opacity,
-				fontSize: fontSize + "px"
+				fontSize: fontSize + "rem"
 			});
 		}
 
@@ -146,9 +185,9 @@
 			var turnAngle = startAngle + idx * spaceAngle;
 
 			var r = center.r;
-			var x = center.x + Math.cos(2 * Math.PI * turnAngle / 360) * (r + 10);
-			var y = center.y - Math.sin(2 * Math.PI * turnAngle / 360) * (r + 10);
-			console.log("turnAngle" + turnAngle, x, y);
+			var x = center.x + Math.cos(2 * Math.PI * turnAngle / 360) * (r );
+			var y = center.y - Math.sin(2 * Math.PI * turnAngle / 360) * (r );
+			// console.log("turnAngle" + turnAngle, x, y);
 			// arc($this.parent().parent().find(".canvas")[0],x,y);
 			//确认方向
 			var dy = 0;
@@ -158,6 +197,12 @@
 			}
 			if (x < center.x) {
 				dx = (x - center.x) / Math.abs(x - center.x)
+			}
+			if(turnAngle%360==90 || turnAngle%360==270){
+				dx=-0.5;
+			}
+			if(turnAngle%360==180 || turnAngle%360==0){
+				dy=-0.5
 			}
 
 			$this.css({
@@ -212,8 +257,10 @@
 				direction: 'vertical'
 				// initialSlide: ,
 			});
+			var maxLen = $(".swiper-slide").length;
 			$(".arrow-top").click(function () {
-				if(vSwiper.activeIndex>=6){
+
+				if(vSwiper.activeIndex>=maxLen-1){
 					vSwiper.slideTo(0);
 				}else{
 					vSwiper.slideTo(vSwiper.activeIndex+1);
@@ -230,8 +277,14 @@
 			},100)
 		});
 
+		var id = 0 ;
+		window.location.search.replace(/id=(\d+)/,function (m,m1) {
+			id=m1;
+		});
+
 		$.ajax({
-			url: "../data.json",
+			// url: "../../data.json",
+			url: "http://lifeapp.flyes.net/stripe.php?id="+id,
 			dataType: "json",
 			success: function (data) {
 				renderData(data)
@@ -240,6 +293,13 @@
 
 		resizeLayout();
 		changeRootrem();
+
+		// $(".likeBtn").click(function () {
+		// 	$.ajax({
+		// 		url:"http://lifeapp.flyes.net/index.php?id=35@&more=click&mobile=33333333330",
+		//
+		// 	})
+		// })
 
 
 	})
